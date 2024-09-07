87228277e59b483291ce5b4f6d7ecb6f1f14ece5
RiedelNicolas <nariedel99@gmail.com>
2024-09-06 21:49:08 -0300

    cambios parciales

--- src/src/app.tsx
+++ src/src/app.tsx
@@ -1,10 +1,21 @@
-import { Container } from "@mui/material";
-
+import {Avatar, Container } from "@mui/material";
+import Grid from '@mui/material/Grid2';
+import './app.css'; 
 
 export function App() {
-
   return (
-    <Container maxWidth="sm">
+    <Container style={{width:"100%"}}>
+      <Grid container spacing={1}>
+      <Grid size={12}>
+        <div className="nameTitle">Nicolás Riedel</div>
+      </Grid>
+      <Grid size={12}>
+        <div className="nameSubtitle">Software Developer</div>
+      </Grid>
+      <Grid size={12}>
+        <Avatar/>
+      </Grid>
+      </Grid>
     </Container>
-  )
+  );
 }
---
+++ src/src/components/Avatar.tsx
@@ -0,0 +1,7 @@
+export function Avatar() {
+    return (
+        <div className="avatarContainer">
+        <img className="avatar" src="https://avatars.githubusercontent.com/u/1011153?v=4" alt="Nicolás Riedel" />
+        </div>
+    );
+    }
